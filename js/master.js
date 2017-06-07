91ee84bda03aa2a4a263a9e86698b42849a7ed79
Joe Romero <joethemovie@gmail.com>
2017-06-07 11:29:17 -0400

    intial commit

---
+++ js/master.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+
+  const expiration =  '6/30/17';
+  const vehicleArray = [];
+
+  function Vehicle(
+     year,
+     make,
+     model,
+     color,
+     doors,
+     engine,
+     transmission,
+     brakes,
+     steering,
+     power,
+     equipment,
+     vin,
+     stock,
+     miles,
+     msrp,
+     buyPrice,
+     leasePrice,
+     residual,
+     security,
+     bankFee,
+     moneyDown,
+     totalPayment,
+     months,
+     milesPerYear,
+     thereAfter,
+     totalAtInception,
+     additionalInfo,
+     vehicleImage,
+     vehicleUrl
+    ){
+      this.year = year;
+      this.make = make;
+      this.model = model;
+      this.color = color;
+      this.doors = doors;
+      this.engine = engine;
+      this.transmission = transmission;
+      this.brakes = brakes;
+      this.steering = steering;
+      this.power = power;
+      this.equipment = equipment;
+      this.vin = vin;
+      this.stock = stock;
+      this.miles = miles;
+      this.msrp = msrp;
+      this.buyPrice = buyPrice;
+      this.leasePrice = leasePrice;
+      this.residual = residual;
+      this.security = security;
+      this.bankFee = bankFee;
+      this.moneyDown = moneyDown;
+      this.totalPayment = totalPayment;
+      this.months = months;
+      this.milesPerYear = milesPerYear;
+      this.thereAfter = thereAfter;
+      this.totalAtInception = totalAtInception;
+      this.additionalInfo = additionalInfo;
+      this.vehicleImage = vehicleImage;
+      this.vehicleUrl = vehicleUrl;
+  }
+
+  function listing(thisCar){
+    document.body.innerHTML += `
+    <div class="specialContainer">
+      <div class="specialContent leftContent">
+        <h3 class="yearMake">${thisCar.year} ${thisCar.make}</h3>
+        <h2 class="modelTrim">${thisCar.model}</h2>
+        <img src="${thisCar.vehicleImage}" alt="volvo vehicle image" class="vehicleImage">
+      </div>
+      <div class="specialContent rightContent">
+      <h3 class="leaseHead">Lease this vehicle for:</h3>
+        <div class="leaseQuoteSection">
+
+          <h1 class="lease">$${thisCar.leasePrice}</h1>
+          <h3 class="monthsSections">/MO
+            <br>
+            ${thisCar.months} MOS
+          </h3>
+        </div>
+        <h4 class="moneyDue">$${thisCar.totalAtInception} DUE AT SIGNING</h4>
+
+        <p class="buyHead">Or buy for: <strong class="buyPrice">$${thisCar.buyPrice}</strong> </p>
+        <a href="${thisCar.vehicleUrl}" class="linkButton" target="_blank">VIEW VEHICLE</a>
+
+        <p class="disclaimer">VIN ${thisCar.vin}. Stk ${thisCar.stock}. ${thisCar.doors}, ${thisCar.engine}, ${thisCar.transmission},mileage: ${thisCar.miles} mi., air conditioning, power steering/ABS brakes/windows/locks/mirrors, cd/MP3, ${thisCar.equipment}. MSRP: $${thisCar.msrp}. $${thisCar.totalAtInception} due at inception. $${thisCar.moneyDown} down + 1st mo pymt $${thisCar.leasePrice}+ bank fee incl. + $0 sec dep. $${thisCar.totalPayment} ttl pymts/$${thisCar.residual} residual.${thisCar.additionalInfo} Prices include all costs to be paid by a consumer except for licensing, registration, taxes, and $299 doc. fee. Tier 1 credit approval thru primary lender. Lessee resp. for excess wear/tear. 7.5K mi. per yr/.25¢ per mi. over thereafter. See dealer for complete details. Not resp. for typos. Offers expire ${expiration}.</p>
+      </div>
+    </div>
+    `
+
+
+
+  }
+
+
+
+  for (var i = 0; i < vehicleArray.length; i++) {
+    listing(vehicleArray[i]);
+  }
+
+
+
+});
